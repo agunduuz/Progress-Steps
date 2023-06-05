e1d7eccae906f59fcaedc0f8fcdc2ed28005af79
Anıl Gündüz <anil.gunduuz@gmail.com>
2023-06-05 22:40:17 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const progress = document.querySelector('#progress');
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const circle = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+next.addEventListener('click', () => {
+  currentActive++;
+  if (currentActive > circle.length) {
+    currentActive = circle.length;
+  }
+
+  update();
+});
+prev.addEventListener('click', () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circle.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  const actives = document.querySelectorAll('.active');
+
+  progress.style.height =
+    ((actives.length - 1) / (circle.length - 1)) * 100 + '%';
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circle.length) {
+    next.disabled = true;
+    prev.disabled = false;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
